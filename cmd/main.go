1880b96768a5725ad645a13f717e0eb63b5e1609
Zane Helton <zane99@me.com>
2024-09-07 16:12:43 -0400

    feat: connect bot to voice channel when url is detected feat: wip lavalink implementation

--- cmd/main.go
+++ cmd/main.go
@@ -12,14 +12,15 @@ import (
 	"uncord-bot-go/config"
 	"uncord-bot-go/handlers"
 
-	// Local module import for handlers
+	"uncord-bot-go/lavalink"
+
 	"github.com/disgoorg/disgo"
 	"github.com/disgoorg/disgo/bot"
 	"github.com/disgoorg/disgo/gateway"
+	"github.com/disgoorg/disgolink/v3/disgolink"
 )
 
 func main() {
-	// Log starting the bot
 	slog.Info("Starting uncord-bot-go...")
 
 	// Load enviornment variables from .env file (for local development)
@@ -29,40 +30,52 @@ func main() {
 	}
 
 	config.LoadConfig() // load configuration
-	config.ConnectDB() // connect to database
+	config.ConnectDB()  // connect to database
 
 	// Create the Disgo client with the appropriate intents and event listener
 	client, err := disgo.New(config.AppConfig.DiscordToken,
 		bot.WithGatewayConfigOpts(
 			gateway.WithIntents(
-				gateway.IntentGuildMessages,    // Listen for guild message events
-				gateway.IntentMessageContent,   // Listen for message content (for reading message text)
-				gateway.IntentGuildMessageReactions,    // Listen for reactions on messages
+				gateway.IntentGuildMessages,         // Listen for guild message events
+				gateway.IntentMessageContent,        // Listen for message content (for reading message text)
+				gateway.IntentGuildMessageReactions, // Listen for reactions on messages
+				gateway.IntentGuildVoiceStates,      // Listen for voice state updates
 			),
 		),
 		bot.WithEventListenerFunc(handlers.OnReactionAdd),
 		bot.WithEventListenerFunc(handlers.OnMessageCreate),
-		bot.WithEventListenerFunc(handlers.OnMessageCreate),
 		bot.WithEventListenerFunc(handlers.OnReactionRemove),
 	)
 	if err != nil {
-		slog.Error("Error while building disgo client", slog.Any("err", err))
+		slog.Error("Error while building client", slog.Any("err", err))
 		return
 	}
 
-	// Ensure the client closes gracefully
+	// Initialize Lavalink client
+	lavalinkClient, err := lavalink.NewClient(disgolink.NodeConfig{
+		Name:     "local",
+		Address:  "localhost:2333",
+		Password: "youshallnotpass",
+		Secure:   false,
+	}, client)
+
+	if err != nil {
+		slog.Error("Error initializing Lavalink client", slog.Any("err", err))
+		return
+	}
+
+	handler := handlers.NewHandler(lavalinkClient, client)
+	client.AddEventListeners(handler)
+
 	defer client.Close(context.TODO())
 
-	// Connect to the Discord gateway
 	if err = client.OpenGateway(context.TODO()); err != nil {
 		slog.Error("Error connecting to Discord gateway", slog.Any("err", err))
 		return
 	}
 
-	// Bot is now running
 	slog.Info("uncord-bot-go is now running. Press CTRL-C to exit.")
 
-	// Listen for termination signals to shut down gracefully
 	s := make(chan os.Signal, 1)
 	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-s
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 )
 
 require (
+	github.com/disgoorg/disgolink/v3 v3.0.2 // indirect
 	github.com/disgoorg/json v1.2.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/sasha-s/go-csync v0.0.0-20240107134140-fcbab37b09ad // indirect
